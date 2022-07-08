7ed53cc0e3508dfbef968f5c9823af76605e4d8b
sanakakadan22 <sanakakadan22@yahoo.com>
2022-07-08 20:37:03 +0200

    remove nextAuth

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -3,12 +3,10 @@ import { createRouter } from "./context";
 import superjson from "superjson";
 
 import { exampleRouter } from "./example";
-import { authRouter } from "./auth";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("auth.", authRouter);
+  .merge("example.", exampleRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
