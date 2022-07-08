4e77b92e8912945804530eaf275e5bb554844bb7
sanakakadan22 <sanakakadan22@yahoo.com>
2022-07-08 23:44:05 +0200

    add .prettierrc

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -1,35 +1,44 @@
 import { Types } from "ably";
 import Ably from "ably/promises";
-import {useEffect} from 'react'
-import {useRouter} from "next/router";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export enum GameEvent {
-    JoinedLobby = 'joinedLobby',
+  JoinedLobby = "joinedLobby",
 }
 
 export const useJoinLobby = (onJoinedLobby: (playerName: string) => void) => {
-    const callback = (message: Types.Message) => { onJoinedLobby(<string>message.data)}
-    useEvent(GameEvent.JoinedLobby, callback)
-}
+  const callback = (message: Types.Message) => {
+    onJoinedLobby(<string>message.data);
+  };
+  useEvent(GameEvent.JoinedLobby, callback);
+};
 
-const events = new Ably.Realtime.Promise({authUrl: '/api/createTokenRequest'});
+const events = new Ably.Realtime.Promise({
+  authUrl: "/api/createTokenRequest",
+});
 
-function useEvent(eventName: string, callbackOnMessage: (message: Types.Message) => void) {
-    const { query } = useRouter();
-    const { code } = query;
+function useEvent(
+  eventName: string,
+  callbackOnMessage: (message: Types.Message) => void
+) {
+  const { query } = useRouter();
+  const { code } = query;
 
-    const useEffectHook = () => {
-        if (!code || typeof code !== 'string') {
-            return
-        }
+  const useEffectHook = () => {
+    if (!code || typeof code !== "string") {
+      return;
+    }
 
-        const channel = events.channels.get(<string>code);
-        channel.subscribe(eventName, msg => {
-            console.log(msg)
-            callbackOnMessage(msg);
-        });
-        return () => { channel.unsubscribe(); };
+    const channel = events.channels.get(<string>code);
+    channel.subscribe(eventName, (msg) => {
+      console.log(msg);
+      callbackOnMessage(msg);
+    });
+    return () => {
+      channel.unsubscribe();
     };
+  };
 
-    useEffect(useEffectHook);
+  useEffect(useEffectHook);
 }
