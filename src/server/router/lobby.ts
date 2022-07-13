0f79bf21eb48754cd8198fcece11eed446bafe58
sanakakadan22 <sanakakadan22@yahoo.com>
2022-07-13 14:58:23 +0200

    add round, choice, answer model to db, and status to lobby create route

--- src/server/router/lobby.ts
+++ src/server/router/lobby.ts
@@ -1,61 +1,71 @@
-import {createRouter} from "./context";
-import {z} from "zod";
-import {nanoid} from "nanoid";
+import { createRouter } from "./context";
+import { z } from "zod";
+import { nanoid } from "nanoid";
 
+export enum GameStatus {
+  InLobby = "inLobby",
+  InGame = "inGame",
+  Ended = "ended",
+}
 export const lobbyRouter = createRouter()
-    .query("get-by-code", {
-        input: z.object({ lobbyCode: z.string() }),
-        async resolve({ ctx , input}) {
-            const lobby = await ctx.prisma.lobby.findFirst({
-                where: {
-                    lobbyCode: input.lobbyCode,
-                },
-                include: {
-                    players: true, // include all players
-                },
-            })
-
-            lobby?.players.forEach(player => player.token = '') // remove all tokens to prevent hacks
-            return lobby ;
+  .query("get-by-code", {
+    input: z.object({ lobbyCode: z.string() }),
+    async resolve({ ctx, input }) {
+      const lobby = await ctx.prisma.lobby.findFirst({
+        where: {
+          lobbyCode: input.lobbyCode,
         },
-    })
-    .mutation("create", {
-        async resolve({ctx, input}) {
-            const name = ctx.req?.cookies["name"]
-            if (!ctx.token || !name) throw new Error("Unauthorized");
-
-            return await ctx.prisma.lobby.create({
-                data: {
-                    lobbyCode: nanoid(5),
-                    players: {
-                        create: [
-                            { name: name, token: ctx.token }, // Populates authorId with user's id
-                        ],
-                    }
-                },
-            });
+        include: {
+          players: true, // include all players
         },
-    })
-    .mutation("join", {
-        input: z.object({ lobbyCode: z.string() }),
-        async resolve({ctx, input}) {
-            const name = ctx.req?.cookies["name"]
-            if (!ctx.token || !name) throw new Error("Unauthorized");
+      });
 
-            const lobby = await ctx.prisma.lobby.update({
-                where: {
-                    lobbyCode: input.lobbyCode,
-                },
-                data: {
-                    players: {
-                        create: [
-                            {name: name, token: ctx.token}, // Populates authorId with user's id
-                        ],
-                    }
-                }
-            })
+      lobby?.players.forEach((player) => (player.token = "")); // remove all tokens to prevent hacks
+      return lobby;
+    },
+  })
+  .mutation("create", {
+    async resolve({ ctx, input }) {
+      const name = ctx.req?.cookies["name"];
+      if (!ctx.token || !name) throw new Error("Unauthorized");
 
-            ctx.events.emitJoinedLobby(input.lobbyCode, name)
-            return lobby
+      return await ctx.prisma.lobby.create({
+        data: {
+          lobbyCode: nanoid(5),
+          status: GameStatus.InLobby,
+          roundLength: 13,
+          players: {
+            create: [
+              {
+                name: name,
+                token: ctx.token,
+              }, // Populates authorId with user's id
+            ],
+          },
         },
-    });
+      });
+    },
+  })
+  .mutation("join", {
+    input: z.object({ lobbyCode: z.string() }),
+    async resolve({ ctx, input }) {
+      const name = ctx.req?.cookies["name"];
+      if (!ctx.token || !name) throw new Error("Unauthorized");
+
+      const lobby = await ctx.prisma.lobby.update({
+        where: {
+          lobbyCode: input.lobbyCode,
+        },
+        data: {
+          players: {
+            create: [
+              { name: name, token: ctx.token }, // Populates authorId with user's id
+            ],
+          },
+        },
+      });
+
+      ctx.events.emitJoinedLobby(input.lobbyCode, name);
+      return lobby;
+    },
+  });
