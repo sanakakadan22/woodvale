c9aadb6b7342bccbd93e98c3bdd279771cd38592
sanakakadan22 <sanakakadan22@yahoo.com>
2022-07-14 19:25:13 +0200

    add score to answer

--- src/server/router/game.ts
+++ src/server/router/game.ts
@@ -1,5 +1,8 @@
 import { createRouter } from "./context";
 import { z } from "zod";
+import is from "@sindresorhus/is";
+import integer = is.integer;
+import { TRPCError } from "@trpc/server";
 
 export const gameRouter = createRouter()
   .mutation("newRound", {
@@ -73,36 +76,98 @@ export const gameRouter = createRouter()
       }); // does this work? Improve later by only selecting player we need
 
       if (lobby == null) {
-        throw new Error("No lobby");
+        throw new TRPCError({
+          code: "NOT_FOUND",
+        });
       }
 
-      const player = lobby.players.filter(
-        (player) => player.token == ctx.token
-      )[0];
-
+      const player = lobby.players.find((player) => player.token == ctx.token);
       if (!player) {
-        throw new Error("No player");
+        throw new TRPCError({
+          code: "NOT_FOUND",
+        });
       }
 
       const round = lobby.rounds[0];
       if (!round) {
-        throw new Error("No round");
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+        });
       }
 
-      if ((Date.now() - round.createdAt.getTime()) / 1000 > lobby.roundLength) {
-        throw new Error("Out of time");
+      const elapsedSeconds = (Date.now() - round.createdAt.getTime()) / 1000;
+      if (elapsedSeconds > lobby.roundLength) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Out of time",
+        });
       }
+
+      const score = Math.round(1 - elapsedSeconds / lobby.roundLength) * 10;
+      console.log(score);
 
       const answer = await ctx.prisma.answer.create({
         data: {
           answer: input.answer,
           roundId: round.id,
           playerId: player.id,
+          score: score,
         },
       });
 
       return {
         correct: answer.answer == round.answer,
       };
+    },
+  })
+  .query("get-round-by-code", {
+    input: z.object({ lobbyCode: z.string() }),
+    async resolve({ ctx, input }) {
+      const lobby = await ctx.prisma.lobby.findFirst({
+        where: {
+          lobbyCode: input.lobbyCode,
+        },
+        include: {
+          rounds: {
+            select: {
+              createdAt: true,
+              question: true,
+              choices: true,
+              answer: true,
+            },
+            orderBy: {
+              createdAt: "desc",
+            },
+            take: 1,
+          },
+          players: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      });
+
+      if (lobby == null) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+        });
+      }
+
+      const round = lobby.rounds[0];
+      if (!round) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+        });
+      }
+
+      if (
+        (Date.now() - round.createdAt.getTime()) / 1000 <=
+        lobby.roundLength
+      ) {
+        round.answer = -1;
+      }
+
+      return lobby;
     },
   });
