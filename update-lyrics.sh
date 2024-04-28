10ecb6e4fb1205401f334c00354826f34d4fc83e
sanakakadan22 <sanakakadan22@yahoo.com>
2024-04-28 02:09:56 -0400

    fix some lyrics

---
+++ update-lyrics.sh
@@ -0,0 +1,9 @@
+curl https://raw.githubusercontent.com/shaynak/taylor-swift-lyrics/main/lyrics.json \
+| sed 's# (Taylor\\u2019s Version)##g' \
+| sed 's# \[From The Vault\]##' \
+| sed '/\"prev\":/d' \
+| sed '/\"next\":/d' \
+| sed '/\"multiplicity\":/d' \
+| sed 's#",#"#' \
+> src/server/lyrics/lyrics.json
+#sed 's/ (Taylor’s Version)//' src/server/lyrics/lyrics.json >> src/server/lyrics/lyrics.json
