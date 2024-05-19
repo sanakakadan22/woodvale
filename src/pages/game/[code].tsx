b2ce70ded05098cf30ad3898b3c52992d0bbad59
sanakakadan22 <sanakakadan22@yahoo.com>
2024-05-19 17:54:18 -0400

    add more confetti

--- src/pages/game/[code].tsx
+++ src/pages/game/[code].tsx
@@ -77,7 +77,6 @@ const GameContent: React.FC<{ lobbyCode: string }> = ({ lobbyCode }) => {
         spread: 100,
         startVelocity: 90,
         ticks: 100,
-        particleCount: 25,
         colors: ["#A79F95", "#78716c", "#f0f0f0", "#1a1f2e", "#06405EFF"],
       });
       confetti({
@@ -85,7 +84,6 @@ const GameContent: React.FC<{ lobbyCode: string }> = ({ lobbyCode }) => {
         startVelocity: 90,
         origin: { x: 0.25, y: 1.2 },
         ticks: 100,
-        particleCount: 25,
         colors: ["#A79F95", "#78716c", "#f0f0f0", "#1a1f2e", "#06405EFF"],
       });
       // and launch a few from the right edge
@@ -93,8 +91,6 @@ const GameContent: React.FC<{ lobbyCode: string }> = ({ lobbyCode }) => {
         spread: 100,
         startVelocity: 90,
         origin: { x: 0.75, y: 1.2 },
-        // gravity: 2,
-        particleCount: 25,
         ticks: 100,
         colors: ["#A79F95", "#78716c", "#f0f0f0", "#1a1f2e", "#06405EFF"],
       });
