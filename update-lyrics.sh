44bfc622396c7debbdfb1dc8c2ee74a3237ab1c5
Sana Pournaghshbande <spournag@amazon.com>
2024-07-04 15:19:18 -0400

    remove from the vault

--- update-lyrics.sh
+++ update-lyrics.sh
@@ -1,7 +1,9 @@
 curl https://raw.githubusercontent.com/shaynak/taylor-swift-lyrics/main/lyrics.json \
 | sed 's# (Taylor\\u2019s Version)##g' \
 | sed "s# (Taylor's Version)##g" \
-| sed 's# \[From The Vault\]##' \
+| sed 's# \[From The Vault\]##g' \
+| sed 's# \[From the Vault\]##g' \
+| sed 's# (From The Vault)##g' \
 | sed '/\"prev\":/d' \
 | sed '/\"next\":/d' \
 | sed '/\"multiplicity\":/d' \
