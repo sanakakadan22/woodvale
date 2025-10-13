d28837cc2a15037d6e08782f92e04d95479686a1
sanakakadan22 <sanakakadan22@yahoo.com>
2025-10-13 17:15:47 -0400

    reduce delay

--- src/pages/game/[code].tsx
+++ src/pages/game/[code].tsx
@@ -68,19 +68,18 @@ const GameContent: React.FC<{ lobbyCode: string }> = ({ lobbyCode }) => {
             volume: 0.5,
         });
 
-        setSpotifyPlayer(player);
-
         player.addListener('ready', ({ device_id }) => {
             console.log('Ready with Device ID', device_id);
             setDeviceId(device_id);
-            setTimeout(() => setSpotifyReady(true), 1000);
+            setSpotifyPlayer(player); 
+            setTimeout(() => setSpotifyReady(true), 100);
         });
 
         player.addListener('not_ready', ({ device_id }) => {
             console.log('Device ID has gone offline', device_id);
         });
 
-        player.connect();
+        player.connect();  
     };
   }, [session?.accessToken]);
 
